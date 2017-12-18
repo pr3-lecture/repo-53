e7896bbaa23c4ccb023ae230c67e4165a75a522e
Thomas Smits <thomsmits@gmail.com>
2017-12-18 22:36:12 +0100

    I Robot

--- koans/about_asserts.rb
+++ koans/about_asserts.rb
@@ -7,13 +7,13 @@ class AboutAsserts < Neo::Koan
 
   # We shall contemplate truth by testing reality, via asserts.
   def test_assert_truth
-    assert true                # This should be true
+    assert false                # This should be true
   end
 
   # Enlightenment may be more easily achieved with appropriate
   # messages.
   def test_assert_with_message
-    assert true, "This should be true -- Please fix this"
+    assert false, "This should be true -- Please fix this"
   end
 
   # To understand reality, we must compare our expectations against
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 Übungen zur Vorlesung PR3.
 
   1. [Ruby Koans](koans)
+  2. [C-Übung: Crypter](crypter)
+
